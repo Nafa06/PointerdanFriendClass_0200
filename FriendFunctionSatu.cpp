63363d959f8f647b4d66ee221c2135f6b9e44bc4
Nafa06 <naufalanggardi09@gmail.com>
2025-05-05 16:05:57 +0700

    Membuat file baru friendfucntionsatu. menambahkan class mahasiswa beserta membernya, membuat fungsi set nama, dan membuat fungsi main

---
+++ FriendFunctionSatu.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+private:
+    string nama;
+
+public:
+    friend void setNama(Mahasiswa &a, string);
+};
+
+void setNama(Mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main(){
+    Mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
